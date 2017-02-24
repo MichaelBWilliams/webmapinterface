a13ed2e9d92c01d920ca4e7638866742586673a8
MichaelBWilliams <ec2-user@ip-172-31-41-77.us-west-2.compute.internal>
2017-02-24 17:50:40 +0000

    working with https

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 from flask import Flask
 from flask import Flask, flash, redirect, render_template, request, session, abort
 import os
+#Added SSLify 
+#from flask_sslify import SSLify
+
 from sqlalchemy.orm import sessionmaker
 from tabledef import *
 engine = create_engine('sqlite:///tutorial.db', echo=True)
+
  
 app = Flask(__name__)
 app.secret_key = os.urandom(12)
+
+#Added path redirect for app feb. 23
+#sslify = SSLify(app)
  
 @app.route('/')
 def home():
@@ -70,7 +77,7 @@ def logout():
  
 if __name__ == "__main__":
 #The following line works for local. Uncomment for local only  
-    app.run(debug=True,host='0.0.0.0', port=4000)
+#    app.run(debug=True,host='0.0.0.0', port=4000)
 #The following line is for app.py to run on EC2 instance.
-#   app.run(debug=True)
+   app.run(debug=True)
 
